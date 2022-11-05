56f06a5bb5d3e6e1d914626d5e5cca89d8c10ddc
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-11-05 15:26:15 +0530

    Replace tsconfig and add expense entity

--- src/entity/Expense.ts
+++ src/entity/Expense.ts
@@ -1,23 +1,30 @@
+import { Field, ObjectType } from "type-graphql"
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
+@ObjectType()
 export class Expense {
+    @Field()
     @PrimaryGeneratedColumn()
-    @Column()
     id: number    
 
+    @Field()
     @Column()
     place: string
     
+    @Field()
     @Column()
     itemName: string
 
+    @Field()
     @Column()
     price: number
 
+    @Field()
     @Column({ default: 1 })
     quantity: number    
 
+    @Field()
     @Column()
     totalPaid: number
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
+import express from 'express';
 import { AppDataSource } from "./data-source"
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { ExpenseResolver } from './resolvers/expense.resolver';
+
+import chalk from 'chalk';
+
+import { PORT } from './constants';
 
 AppDataSource.initialize()
-.then(async () => {
+    .then(async () => {
+        const app = express();
 
-}).catch(error => console.log(error))
+        const apolloServer = new ApolloServer({
+            schema: await buildSchema({
+                resolvers: [ExpenseResolver],
+                validate: false
+            })
+        });
+
+        await apolloServer.start();
+        apolloServer.applyMiddleware({
+            app
+        })
+
+        app.listen(PORT, () => {
+            console.log(chalk.blue(`Server is running on PORT ${PORT}`));
+        });
+
+    }).catch(error => console.log("DB::>", error))
