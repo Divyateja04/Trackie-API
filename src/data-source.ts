6b0ce998fc21290d8d60887abfa4ab446beff9bd
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-11-13 20:19:38 +0530

    change env variables for postgres

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,10 +7,10 @@ import { Subject } from './entity/Subject';
 
 export const db = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
+    host: process.env.PGHOST || "localhost",
+    port: Number(process.env.PGPORT) || 5432,
+    username: process.env.PGUSER || "postgres",
+    password: process.env.PGPASSWORD || "postgres",
     database: "trackie",
     dropSchema: false,
     synchronize: true,
